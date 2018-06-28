1b49bed6b3f16edf236f926cdeba11eedf659a45
Julie P <Julie@Austins-iMac.local>
2018-06-27 17:18:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// var app = new Vue({ 
+//   el: '#app',
+//   data: {
+//       message: 'Hello Vue!'
+//   }
+// });
+
+var app = new Vue({ 
+  el: '#app',
+  data: {
+      seen: true
+  }
+});
