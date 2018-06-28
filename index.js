78ce0a1785700fba076945101fd7d8caa64a8b35
Julie P <Julie@Austins-iMac.local>
2018-06-27 17:55:13 -0700

    components

--- index.js
+++ index.js
@@ -44,3 +44,20 @@ var app5 = new Vue({
   }
 });
 
+// registering a vue component.  name followed by options object with a template
+Vue.component('tudo-item', {
+    props: ['tudo'],
+    template: '<li>{{ tudo.text }}</li>'
+})
+
+var app6 = new Vue({ 
+  el: '#app6',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Veggies' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Meat'}
+    ]
+  }
+});
+//ideally we want to pass data from var app6 into the Vue.component to keep adding new templates to the list.
